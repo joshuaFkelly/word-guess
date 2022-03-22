d266cd9214b99b65cb8203aa4b4a606d1553b296
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-22 11:06:47 -0400

    created variables for the game

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("test");
 const winsEl = document.querySelector("#wins");
 const guessesEl = document.querySelector("#guesses");
 const guessedEl = document.querySelector("#guessed");
+let wins = 0;
+let guesses = 10;
+let guessed = [];
