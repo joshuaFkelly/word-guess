f1535c234e1ee056083aa25106a8989baf936721
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-22 11:17:53 -0400

    added images, created a words object that hold each word and its corresponding picture, added a current word h3 to the html and selected it using js and saved to a variable for future use

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ console.log("test");
 const winsEl = document.querySelector("#wins");
 const guessesEl = document.querySelector("#guesses");
 const guessedEl = document.querySelector("#guessed");
+const currentWordEl = document.querySelector("#currentWord");
 let wins = 0;
 let guesses = 10;
 let guessed = [];
+const words = [
+  {
+    word: "armbar",
+    image: "images/Armbar.jpg",
+  },
+  {
+    word: "triangle",
+    image: "images/Triangle.jpg",
+  },
+  {
+    word: "kimura",
+    image: "images/Kimura.jpg",
+  },
+  {
+    word: "kneebar",
+    image: "images/Kneebar.jpg",
+  },
+];
